c1c44cb17568c3e36252e3ea467d1fd8775afa9c
tianma8023 <1808931265@qq.com>
2018-07-23 19:16:22 +0800

    [Optimize] Use BroadcastReceiver to control the lifecycle of the Accessibility rather than listening system clipboard. [Enhance] Enhance the function of finding the exact SMS code EditText.

--- app/src/main/java/com/github/tianma8023/xposed/smscode/service/accessibility/SmsCodeAutoInputService.java
+++ app/src/main/java/com/github/tianma8023/xposed/smscode/service/accessibility/SmsCodeAutoInputService.java
@@ -1,115 +1,234 @@
 package com.github.tianma8023.xposed.smscode.service.accessibility;
 
-import android.content.ClipData;
-import android.content.ClipDescription;
-import android.content.ClipboardManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.EditText;
 
+import com.crossbowffs.remotepreferences.RemotePreferences;
+import com.github.tianma8023.xposed.smscode.constant.IPrefConstants;
+import com.github.tianma8023.xposed.smscode.utils.AccessibilityUtils;
+import com.github.tianma8023.xposed.smscode.utils.RemotePreferencesUtils;
+import com.github.tianma8023.xposed.smscode.utils.ShellUtils;
 import com.github.tianma8023.xposed.smscode.utils.VerificationUtils;
 import com.github.tianma8023.xposed.smscode.utils.XLog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * An accessibility service that can input verification code automatically.
+ * An accessibility service that can input SMS code automatically.
  */
 public class SmsCodeAutoInputService extends BaseAccessibilityService {
 
-    private ClipboardManager mClipboardManager;
+    private RemotePreferences mPreferences;
+
+    public static final String ACTION_START_AUTO_INPUT = "action_start_auto_input";
+    public static final String ACTION_STOP_AUTO_INPUT_SERVICE = "action_stop_auto_input_service";
+
+    public static final String EXTRA_KEY_SMS_CODE = "extra_key_sms_code";
+
+    private class AutoInputControllerReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            XLog.d("AutoInputControllerReceiver action=%s", action);
+            if (ACTION_START_AUTO_INPUT.equals(action)) {
+                String smsCode = intent.getStringExtra(EXTRA_KEY_SMS_CODE);
+                autoInputSmsCode(smsCode);
+            } else if (ACTION_STOP_AUTO_INPUT_SERVICE.equals(action)) {
+                if (RemotePreferencesUtils.getBooleanPref(mPreferences, IPrefConstants.KEY_AUTO_INPUT_MODE_ROOT, IPrefConstants.KEY_AUTO_INPUT_MODE_ROOT_DEFAULT)) {
+                    boolean result = ShellUtils.disableAccessibilityService(
+                            AccessibilityUtils.getAccessibilityServiceName(SmsCodeAutoInputService.class));
+                    XLog.d("disable service = " + (result ? "succeed" : "failed"));
+                }
+            }
+        }
+    }
+
+    private AutoInputControllerReceiver mControllerReceiver;
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-        initClipboardManager();
+        init();
+    }
+
+    private void init() {
+        if (mPreferences == null) {
+            mPreferences = RemotePreferencesUtils.getDefaultRemotePreferences(this.getApplicationContext());
+        }
+
+        if (mControllerReceiver == null) {
+            mControllerReceiver = new AutoInputControllerReceiver();
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction(ACTION_START_AUTO_INPUT);
+            intentFilter.addAction(ACTION_STOP_AUTO_INPUT_SERVICE);
+            registerReceiver(mControllerReceiver, intentFilter);
+        }
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
     }
 
-    private void initClipboardManager() {
-        if (mClipboardManager == null) {
-            mClipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+    @Override
+    public void onDestroy() {
+        if (mControllerReceiver != null) {
+            unregisterReceiver(mControllerReceiver);
         }
-        if (mClipboardManager != null) {
-            mClipboardManager.addPrimaryClipChangedListener(mPrimaryClipChangedListener);
+        super.onDestroy();
+    }
+
+    private void autoInputSmsCode(String smsCode) {
+        tryToAutoInputSMSCode(smsCode);
+
+        Intent stopAutoInput = new Intent();
+        stopAutoInput.setAction(ACTION_STOP_AUTO_INPUT_SERVICE);
+        sendBroadcast(stopAutoInput);
+    }
+
+    private void tryToAutoInputSMSCode(String smsCode) {
+        AccessibilityNodeInfo rootNodeInfo = getRootInActiveWindow();
+        if (rootNodeInfo == null) {
+            return;
+        }
+        try {
+            List<AccessibilityNodeInfo> editTextNodes = new ArrayList<>();
+            traverse(rootNodeInfo, editTextNodes);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                autoInputSinceOreo(editTextNodes, smsCode);
+            } else {
+                autoInputBeforeOreo(editTextNodes, smsCode);
+            }
+        } catch (Exception e) {
+            XLog.e("error occurs in traverse()", e);
         }
     }
 
-    private ClipboardManager.OnPrimaryClipChangedListener mPrimaryClipChangedListener = new ClipboardManager.OnPrimaryClipChangedListener() {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void autoInputSinceOreo(List<AccessibilityNodeInfo> editTextNodes, String smsCode) {
+        // 判断有没有验证码输入框
+        for (AccessibilityNodeInfo nodeInfo : editTextNodes) {
+            if (nodeInfo.isFocusable()) {
+                CharSequence hintSequence = nodeInfo.getHintText();
+                if (hintSequence == null) {
+                    continue;
+                }
+                String hint = hintSequence.toString();
 
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        @Override
-        public void onPrimaryClipChanged() {
-            if (mClipboardManager.hasPrimaryClip()) {
-                ClipData clipData = mClipboardManager.getPrimaryClip();
-                if (clipData.getDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                    ClipData.Item item = clipData.getItemAt(0);
-                    onNewTextCopied(item.getText().toString());
+                boolean flag = VerificationUtils.containsVerificationKeywords(getApplicationContext(), hint);
+                if (flag) {
+                    // 模拟输入
+                    inputText(nodeInfo, smsCode);
+                    XLog.d("SMS code EditText found!");
+                    return;
                 }
             }
         }
-    };
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void onNewTextCopied(String textCopied) {
-//        if (VerificationUtils.maybeVerificationCode(textCopied)) {
-            AccessibilityNodeInfo rootNodeInfo = getRootInActiveWindow();
-            if (rootNodeInfo != null) {
-                try {
-                    traverse(rootNodeInfo, textCopied);
-                } catch (Exception e) {
-                    XLog.e("error occurs in traverse ", e);
+        if (editTextNodes.size() == 1) { // 只有一个EditText节点
+            XLog.d("Have 1 EditText node");
+            AccessibilityNodeInfo smsCodeNode = editTextNodes.get(0);
+            inputText(smsCodeNode, smsCode);
+        } else if (editTextNodes.size() == 2) { // 有两个EditText (一个是电话号码,一个是验证码输入框)
+            XLog.d("Have 2 EditText nodes");
+            AccessibilityNodeInfo phoneNumberNode = editTextNodes.get(0);
+            AccessibilityNodeInfo smsCodeNode = editTextNodes.get(1);
+            CharSequence pnHintSequence = phoneNumberNode.getHintText();
+            if (!TextUtils.isEmpty(pnHintSequence)) {
+                if (VerificationUtils.containsPhoneNumberKeywords(pnHintSequence.toString())) {
+                    inputText(smsCodeNode, smsCode);
+                    return;
                 }
             }
-//        }
-
-//        boolean result = ShellUtils.disableAccessibilityService(
-//                AccessibilityUtils.getAccessibilityServiceName(SmsCodeAutoInputService.class));
-//        XLog.i("disable service = " + (result ? "succeed" : "failed"));
+            CharSequence pnTextSequence = phoneNumberNode.getText();
+            if (!TextUtils.isEmpty(pnTextSequence)) {
+                if (VerificationUtils.isPossiblePhoneNumber(pnTextSequence.toString())) {
+                    inputText(smsCodeNode, smsCode);
+                }
+            }
+        }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void traverse(AccessibilityNodeInfo nodeInfo, String textToPaste) {
-        if (nodeInfo.getChildCount() == 0) {
-            handleLeafNodeInfo(nodeInfo, textToPaste);
+    private void autoInputBeforeOreo(List<AccessibilityNodeInfo> editTextNodes, String smsCode) {
+        // 判断有没有验证码输入框
+        for (AccessibilityNodeInfo nodeInfo : editTextNodes) {
+            if (nodeInfo.isFocusable()) {
+                CharSequence text = nodeInfo.getText();
+                if (text == null) {
+                    continue;
+                }
+                String hintOrText = text.toString();
+
+                boolean flag = VerificationUtils.containsVerificationKeywords(getApplicationContext(), hintOrText);
+                if (flag) {
+                    // 模拟输入
+                    inputText(nodeInfo, smsCode);
+                    XLog.d("SMS code EditText found!");
+                    return;
+                }
+            }
+        }
+
+        if (editTextNodes.size() == 1) { // 只有一个EditText节点
+            AccessibilityNodeInfo smsCodeNode = editTextNodes.get(0);
+            inputText(smsCodeNode, smsCode);
+        } else if (editTextNodes.size() == 2) { // 有两个EditText (一个是电话号码,一个是验证码输入框)
+            AccessibilityNodeInfo phoneNumberNode = editTextNodes.get(0);
+            AccessibilityNodeInfo smsCodeNode = editTextNodes.get(1);
+            CharSequence pnTextSequence = phoneNumberNode.getText();
+            if (!TextUtils.isEmpty(pnTextSequence)) {
+                if (VerificationUtils.isPossiblePhoneNumber(pnTextSequence.toString())) {
+                    inputText(smsCodeNode, smsCode);
+                }
+            }
+        }
+    }
+
+    /**
+     * 遍历以当前 nodeInfo 为根节点的节点树，将EditText节点存储在editTextNodes中
+     *
+     * @param nodeInfo      current root node
+     * @param editTextNodes store EditText nodes.
+     */
+    private void traverse(AccessibilityNodeInfo nodeInfo,
+                          List<AccessibilityNodeInfo> editTextNodes) {
+        if (nodeInfo.getChildCount() == 0) { // 叶子节点
+            handleLeafNodeInfo(nodeInfo, editTextNodes);
         } else {
             for (int i = 0; i < nodeInfo.getChildCount(); i++) {
                 AccessibilityNodeInfo childNodeInfo = nodeInfo.getChild(i);
                 if (childNodeInfo != null) {
-                    traverse(childNodeInfo, textToPaste);
+                    traverse(childNodeInfo, editTextNodes);
                 }
             }
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void handleLeafNodeInfo(AccessibilityNodeInfo nodeInfo, String textToPaste) {
+    /**
+     * 处理叶节点，返回是否找到该输入验证码的EditText
+     *
+     * @param nodeInfo      nodeInfo
+     * @param editTextNodes editTextNodes
+     */
+    private void handleLeafNodeInfo(AccessibilityNodeInfo nodeInfo,
+                                    List<AccessibilityNodeInfo> editTextNodes) {
         try {
             Class<?> clz = Class.forName(nodeInfo.getClassName().toString());
-            XLog.i("class=%s, text=%s, hint=%s, editable=%s, contentDescription=%s",
+            XLog.d("class=%s, text=%s",
                     nodeInfo.getClassName().toString(),
-                    nodeInfo.getText(),
-                    nodeInfo.getHintText(),
-                    String.valueOf(nodeInfo.isEditable()),
-                    nodeInfo.getContentDescription());
+                    nodeInfo.getText());
             if (EditText.class.isAssignableFrom(clz)) { // is EditText
-                if (nodeInfo.isFocusable()) {
-                    CharSequence text = nodeInfo.getText();
-                    if (text == null) {
-                        return;
-                    }
-                    String hintOrText = text.toString();
-
-                    boolean flag = VerificationUtils.containsVerificationKeywords(getApplicationContext(), hintOrText);
-                    if (flag) {
-                        // 模拟粘贴
-                        nodeInfo.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
-                        nodeInfo.performAction(AccessibilityNodeInfo.ACTION_PASTE);
-                    }
-                }
+                editTextNodes.add(nodeInfo);
             }
         } catch (ClassNotFoundException e) {
             // ignore
